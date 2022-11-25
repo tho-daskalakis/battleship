6c70f756b5ad7da27fc6185d94f44e4f5ed44100
Thomas <tho.daskalakis@gmail.com>
2022-11-25 17:35:50 +0200

    Setup JEST with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: 'asset/resource',
+      },
     ],
   },
 };
