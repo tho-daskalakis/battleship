11ffd3abaed6d7cf2f312bd9e346295aba51a804
Thomas <tho.daskalakis@gmail.com>
2022-11-26 12:34:14 +0200

    Fix typescript in webpack integration

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import './index.scss';
 
 console.log('hello!');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Hello Battleship!';
+
+const app = document.getElementById('app') as HTMLDivElement;
+app.appendChild(h1);
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
@@ -19,5 +24,8 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
