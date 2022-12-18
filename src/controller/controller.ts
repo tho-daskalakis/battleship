6f072f361603b5abbd6f8b22b35c42eb056d32b7
Thomas <tho.daskalakis@gmail.com>
2022-12-18 15:36:01 +0200

    Implement visual cues for player ships on the board

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -7,9 +7,11 @@ const players = [playerFactory('Player'), computerFactory('Computer')];
 function newGame() {
   clearApp();
   createMainPage();
+  initPlayers();
 
   // For testing
   placeShipsToDefaultPositions();
+  showShipsOnBoard();
 }
 
 function clearApp() {
@@ -25,6 +27,11 @@ function clearApp() {
   body.appendChild(app);
 }
 
+function initPlayers() {
+  players[0].init();
+  players[1].init();
+}
+
 function placeShipsToDefaultPositions() {
   // Place ships for player
   const player = players[0];
@@ -36,6 +43,53 @@ function placeShipsToDefaultPositions() {
   playerBoard.placeShip({ x: 1, y: 'C' }, 2, false);
   playerBoard.placeShip({ x: 1, y: 'D' }, 3, false);
   playerBoard.placeShip({ x: 1, y: 'E' }, 4, false);
+}
+
+/**
+ * Display player's ships on board. Color the tiles that contain a ship,
+ * and display the ship name's first letter on the tiles.
+ */
+function showShipsOnBoard() {
+  const player = players[0];
+  const playerBoard = player.getBoard();
+  const tileGrid = playerBoard.getBoard();
+
+  // Get DOM representation of the grid
+  const domGrid = document.querySelector(
+    '#player-board > .board-grid'
+  ) as HTMLDivElement;
+  // console.log(domGrid);
+  // console.log(domGrid.childNodes);
+  // console.log(domGrid.children);
+  const tileList = [...domGrid.childNodes];
+
+  tileGrid.forEach((col) => {
+    col.forEach((tile) => {
+      // If tile has ship, color it
+      if (tile.getShipIndex() !== null) {
+        const x = tile.coords.x;
+        const y = tile.coords.y;
+        console.log(x, y);
+
+        const currentTile = tileList.find((element) => {
+          if (!(element instanceof HTMLElement)) return undefined;
+          // console.log(element.dataset.xCoord);
+
+          if (
+            element.dataset.xCoord === x.toString() &&
+            element.dataset.yCoord === y.toString()
+          ) {
+            return element;
+          }
+        });
+
+        if (currentTile instanceof HTMLDivElement) {
+          currentTile.textContent = 'S';
+          currentTile.classList.add('has-ship');
+        }
+      }
+    });
+  });
 }
 
 export { newGame };
